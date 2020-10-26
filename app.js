d6bdc8053ab57a2f87c1d2e8f3e36b6a07645106
mihirangap <pathum@silentiuminternational.com.au>
2020-10-26 21:48:09 +0530

    log parsed xml

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser')
 
 var indexRouter = require('./routes/index');
 
@@ -12,30 +13,11 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.text());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
 app.listen(port, address, () => console.log("Server Started. Running on localhost in 3000"))
-
-// const coap = require('coap')
-// var server = coap.createServer()
-
-// server.on('request', function (req, res) {
-//   // console.log(req.payload.toString());
-//   res.end('Hello NodeMCU ' + req.url.split('/')[1] + '\n')
-// })
-
-// server.listen(function () {
-//   var req = coap.request('coap://localhost');
-//   console.log('Listening on : 5683')
-
-//   req.on('response', function (res) {
-//     res.pipe(process.stdout)
-//   });
-
-//   req.end()
-
-// })
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+var parseString = require('xml2js').parseString;
+
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/cap-submit', function (req, res, next) {
+  console.log(req.body);
+  parseString(req.body, function (err, result) {
+    if (err) {
+      console.log(JSON.stringify(err, null, 2));
+      res.status(400).send();
+    } else {
+      console.log(JSON.stringify(result, null, 2));
+      res.status(200).end();
+    }
+    console.log("\n\n\n\n\n")
+  });
 });
 
 module.exports = router;
